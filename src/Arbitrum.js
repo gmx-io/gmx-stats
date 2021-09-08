5a08876cd08fd1a05921390a1da8a941b32ca472
Georgy Krasulya <gkrasulya@gmail.com>
2021-09-08 10:37:21 +0300

    update arbitrum charts

--- src/Arbitrum.js
+++ src/Arbitrum.js
@@ -47,27 +47,36 @@ const data = [
   }
 ]
 
-const numberFmt = Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' })
+const currencyFmt = Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' })
+const numberFmt = Intl.NumberFormat('en-US')
 const NOW = Math.floor(Date.now() / 1000)
 
-const formatUsdValue = value => {
+const formatNumberValue = value => {
     if (value >= 1e9) {
-      return `$${(value / 1e9).toFixed(value < 1e10 ? 2 : 1)}B`
+      return `${(value / 1e9).toFixed(value < 1e10 ? 2 : 1)}B`
     }
     if (value >= 1e6) {
-      return `$${(value / 1e6).toFixed(value < 1e7 ? 2 : 1)}M`
+      return `${(value / 1e6).toFixed(value < 1e7 ? 2 : 1)}M`
     }
     if (value >= 1e3) {
-      return `$${(value / 1e3).toFixed(value < 1e4 ? 2 : 1)}K`
+      return `${(value / 1e3).toFixed(value < 1e4 ? 2 : 1)}K`
     }
-    return `$${value.toFixed(1)}`
+    return `${value.toFixed(1)}`
+}
+
+const formatUsdValue = value => {
+  return `$${formatNumberValue(value)}`
+}
+
+const tooltipFormatterNumber = (value, name, item) => {
+  return numberFmt.format(value)
 }
 
 const tooltipFormatter = (value, name, item) => {
   if (item && item.unit === '%') {
     return value.toFixed(2)
   }
-  return numberFmt.format(value)
+  return currencyFmt.format(value)
 }
 
 const GRAPH_API_URL = "https://api.thegraph.com/subgraphs/name/gkrasulya/gmx";
@@ -149,6 +158,10 @@ function Arbitrum() {
     }, [])
   }, [data])
 
+  const yaxisFormatterNumber = useCallback(value => {
+    return formatNumberValue(value)
+  })
+
   const yaxisFormatter = useCallback((value, ...args) => {
     if (displayPercentage) {
       return value.toFixed(2)
@@ -172,7 +185,7 @@ function Arbitrum() {
       if (item && item.unit === '%') {
         return date
       }
-      return `${date}, ${numberFmt.format(all)}`
+      return `${date}, ${currencyFmt.format(all)}`
     }
     return date
   }, [period])
@@ -211,31 +224,13 @@ function Arbitrum() {
       <h1>GMX analytics / Arbitrum</h1>
       <div className="chart-grid">
         <div className="chart-cell half">
-          <h3>AUM</h3>
-          { loading && <RiLoader5Fill size="3em" className="loader" /> }
-          <ResponsiveContainer width="100%" height={CHART_HEIGHT}>
-            <BarChart syncId="syncId" data={addLiquiditiesChartData}>
-              <CartesianGrid strokeDasharray="10 10" />
-              <XAxis dataKey="timestamp" tickFormatter={tooltipLabelFormatter} minTickGap={30} />
-              <YAxis dataKey="aum" unit={dynamicUnit} tickFormatter={yaxisFormatter} width={YAXIS_WIDTH} />
-              <Tooltip
-                formatter={tooltipFormatter}
-                labelFormatter={tooltipLabelFormatter}
-                contentStyle={{ textAlign: 'left' }}
-              />
-              <Legend />
-              <Bar type="monotone" unit={dynamicUnit} dataKey="aum" stackId="a" name="AUM" fill="#ee64b8" />
-            </BarChart>
-          </ResponsiveContainer>
-        </div>
-        <div className="chart-cell half">
-          <h3>Glp Price / Supply</h3>
+          <h3>AUM / Glp Price</h3>
           { loading && <RiLoader5Fill size="3em" className="loader" /> }
           <ResponsiveContainer width="100%" height={CHART_HEIGHT}>
             <ComposedChart syncId="syncId" data={addLiquiditiesChartData}>
               <CartesianGrid strokeDasharray="10 10" />
               <XAxis dataKey="timestamp" tickFormatter={tooltipLabelFormatter} minTickGap={30} />
-              <YAxis dataKey="glpSupply" unit={dynamicUnit} tickFormatter={yaxisFormatter} width={YAXIS_WIDTH} />
+              <YAxis dataKey="aum" unit={dynamicUnit} tickFormatter={yaxisFormatter} width={YAXIS_WIDTH} />
               <YAxis orientation="right" dataKey="glpPrice" tickFormatter={yaxisFormatter} yAxisId="right" width={YAXIS_WIDTH} />
               <Tooltip
                 formatter={tooltipFormatter}
@@ -243,9 +238,27 @@ function Arbitrum() {
                 contentStyle={{ textAlign: 'left' }}
               />
               <Legend />
+              <Bar type="monotone" unit={dynamicUnit} dataKey="aum" stackId="a" name="AUM" fill="#ee64b8" />
               <Line type="monotone" yAxisId="right" unit={dynamicUnit} dot={false} dataKey="glpPrice" stackId="a" name="GLP Price" stroke="#c90000" />
-              <Bar type="monotone" unit={dynamicUnit} dataKey="glpSupply" stackId="a" name="GLP Supply" fill="#8884ff" />
             </ComposedChart>
+          </ResponsiveContainer>
+        </div>
+        <div className="chart-cell half">
+          <h3>Glp Supply</h3>
+          { loading && <RiLoader5Fill size="3em" className="loader" /> }
+          <ResponsiveContainer width="100%" height={CHART_HEIGHT}>
+            <BarChart syncId="syncId" data={addLiquiditiesChartData}>
+              <CartesianGrid strokeDasharray="10 10" />
+              <XAxis dataKey="timestamp" tickFormatter={tooltipLabelFormatter} minTickGap={30} />
+              <YAxis dataKey="glpSupply" tickFormatter={yaxisFormatterNumber} width={YAXIS_WIDTH} />
+              <Tooltip
+                formatter={tooltipFormatterNumber}
+                labelFormatter={tooltipLabelFormatter}
+                contentStyle={{ textAlign: 'left' }}
+              />
+              <Legend />
+              <Bar type="monotone" dataKey="glpSupply" stackId="a" name="GLP Supply" fill="#8884ff" />
+            </BarChart>
           </ResponsiveContainer>
         </div>
       </div>
