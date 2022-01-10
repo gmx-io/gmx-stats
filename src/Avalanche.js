1da8b7cfa02d51d15e8a05ae02bb1cf7325f4bb8
gdev8317 <95585424+gdev8317@users.noreply.github.com>
2022-01-11 00:15:19 +0300

    support other charts on avalanche

--- src/Avalanche.js
+++ src/Avalanche.js
@@ -88,7 +88,8 @@ function Arbitrum() {
 
   const params = { from, to, chainName: 'avalanche' }
 
-  // const [fundingRateData, fundingRateLoading] = useFundingRateData(params)
+  const [fundingRateData, fundingRateLoading] = useFundingRateData(params)
+
   const [volumeData, volumeLoading] = useVolumeData(params)
   // const [totalVolume] = useTotalVolumeFromServer()
   const [totalVolume, totalVolumeDelta] = useMemo(() => {
@@ -147,14 +148,14 @@ function Arbitrum() {
     ]
   }, [usersData])
 
-  // const [swapSources, swapSourcesLoading] = useSwapSources(params)
-  // const swapSourcesKeys = Object.keys((swapSources || []).reduce((memo, el) => {
-  //   Object.keys(el).forEach(key => {
-  //     if (key === 'all' || key === 'timestamp') return
-  //     memo[key] = true
-  //   })
-  //   return memo
-  // }, {}))
+  const [swapSources, swapSourcesLoading] = useSwapSources(params)
+  const swapSourcesKeys = Object.keys((swapSources || []).reduce((memo, el) => {
+    Object.keys(el).forEach(key => {
+      if (key === 'all' || key === 'timestamp') return
+      memo[key] = true
+    })
+    return memo
+  }, {}))
 
   const [lastSubgraphBlock] = useLastSubgraphBlock(params.chainName)
   const [lastBlock] = useLastBlock(params.chainName)
@@ -279,7 +280,7 @@ function Arbitrum() {
         </div>
         <div className="chart-cell">
           <ChartWrapper
-            title="Glp Price"
+            title="Glp Price Comparison"
             loading={glpLoading}
             data={glpPerformanceData}
             csvFields={[{key: 'syntheticPrice'}, {key: 'glpPrice'}, {key: 'glpPlusFees'}, {key: 'lpBtcPrice'}, {key: 'lpEthPrice'}]}
@@ -288,18 +289,111 @@ function Arbitrum() {
               <LineChart data={glpPerformanceData} syncId="syncGlp">
                 <CartesianGrid strokeDasharray="10 10" />
                 <XAxis dataKey="timestamp" tickFormatter={tooltipLabelFormatter} minTickGap={30} />
-                <YAxis dataKey="glpPrice" domain={[0.4, 1.7]} tickFormatter={yaxisFormatterNumber} width={YAXIS_WIDTH} />
+                <YAxis dataKey="performanceSyntheticCollectedFees" domain={[60, 210]} unit="%" tickFormatter={yaxisFormatterNumber} width={YAXIS_WIDTH} />
+                <YAxis dataKey="glpPrice" domain={[0.4, 1.7]} orientation="right" yAxisId="right" tickFormatter={yaxisFormatterNumber} width={YAXIS_WIDTH} />
                 <Tooltip
                   formatter={tooltipFormatterNumber}
                   labelFormatter={tooltipLabelFormatter}
                   contentStyle={{ textAlign: 'left' }}
                 />
                 <Legend />
-                <Line isAnimationActive={false} type="monotone" unit="$" strokeWidth={1} dot={false} dataKey="glpPrice" name="Glp Price" stroke={COLORS[1]} strokeWidth={1} />
-                <Line isAnimationActive={false} type="monotone" unit="$" strokeWidth={1} dot={false} dataKey="glpPlusFees" name="Glp w/ fees" stroke={COLORS[3]} strokeWidth={1} />
+                <Line dot={false} isAnimationActive={false} type="monotone" unit="%" strokeWidth={2} dataKey="performanceLpBtcCollectedFees" name="% LP BTC-USDC (w/ fees)" stroke={COLORS[2]} />
+                <Line dot={false} isAnimationActive={false} type="monotone" unit="%" strokeWidth={2} dataKey="performanceLpEthCollectedFees" name="% LP ETH-USDC (w/ fees)" stroke={COLORS[4]} />
+                <Line dot={false} isAnimationActive={false} type="monotone" unit="%" strokeWidth={2} dataKey="performanceSyntheticCollectedFees" name="% Index (w/ fees)" stroke={COLORS[0]} />
+
+                <Line isAnimationActive={false} type="monotone" unit="$" strokeWidth={1} yAxisId="right" dot={false} dataKey="syntheticPrice" name="Index Price" stroke={COLORS[2]} />
+                <Line isAnimationActive={false} type="monotone" yAxisId="right" unit="$" strokeWidth={1} dot={false} dataKey="glpPrice" name="Glp Price" stroke={COLORS[1]} strokeWidth={1} />
+                <Line isAnimationActive={false} type="monotone" yAxisId="right" unit="$" strokeWidth={1} dot={false} dataKey="glpPlusFees" name="Glp w/ fees" stroke={COLORS[3]} strokeWidth={1} />
+                <Line isAnimationActive={false} type="monotone" unit="$" strokeWidth={1} yAxisId="right" dot={false} dataKey="lpBtcPrice" name="LP BTC-USDC" stroke={COLORS[2]} />
+                <Line isAnimationActive={false} type="monotone" unit="$" strokeWidth={1} yAxisId="right" dot={false} dataKey="lpEthPrice" name="LP ETH-USDC" stroke={COLORS[4]} />
+                <Line isAnimationActive={false} type="monotone" unit="$" strokeWidth={1} yAxisId="right" dot={false} dataKey="lpAvaxPrice" name="LP AVAX-USDC" stroke={COLORS[5]} />
               </LineChart>
             </ResponsiveContainer>
+            <div className="chart-description">
+              <p>
+                <span style={{color: COLORS[3]}}>Glp with fees</span> is based on GLP share of fees received and excluding esGMX rewards<br/>
+                <span style={{color: COLORS[0]}}>% of Index (with fees)</span> is Glp with fees / Index Price * 100<br/>
+                <span style={{color: COLORS[4]}}>% of LP TOKEN-USDC (with fees)</span> is Glp Price with fees / LP TOKEN-USDC * 100<br/>
+                <span style={{color: COLORS[2]}}>Index Price</span> is 25% BTC, 25% ETH, 50% USDC
+              </p>
+            </div>
           </ChartWrapper>
+        </div>
+        <div className="chart-cell">
+          <ChartWrapper
+            title="Traders Net PnL"
+            loading={tradersLoading}
+            data={tradersData?.data}
+            csvFields={[{key: 'pnl', name: 'Net PnL'}, {key: 'pnlCumulative', name: 'Cumulative PnL'}]}
+          >
+            <ResponsiveContainer width="100%" syncId="tradersId" height={CHART_HEIGHT}>
+              <ComposedChart data={tradersData?.data}>
+                <CartesianGrid strokeDasharray="10 10" />
+                <XAxis dataKey="timestamp" tickFormatter={tooltipLabelFormatter} minTickGap={30} />
+                <YAxis domain={[-tradersData?.stats.maxAbsOfPnlAndCumulativePnl * 1.05, tradersData?.stats.maxAbsOfPnlAndCumulativePnl * 1.05]} tickFormatter={yaxisFormatter} width={YAXIS_WIDTH} />
+                <Tooltip
+                  formatter={tooltipFormatter}
+                  labelFormatter={tooltipLabelFormatter}
+                  contentStyle={{ textAlign: 'left' }}
+                />
+                <Legend />
+                <Bar type="monotone" fill="#444" dot={false} dataKey="pnl" name="Net PnL">
+                  {(tradersData?.data || []).map((item, i) => {
+                    return <Cell key={`cell-${i}`} fill={item.pnl > 0 ? '#22c761' : '#f93333'} />
+                  })}
+                </Bar>
+                <Line type="monotone" strokeWidth={2} stroke="#8884ff" dataKey="pnlCumulative" name="Cumulative PnL" />
+              </ComposedChart>
+            </ResponsiveContainer>
+            <div className="chart-description">
+              <p>Considers settled (closed) positions</p>
+              <p>Fees are not factored into PnL</p>
+            </div>
+          </ChartWrapper>
+        </div>
+        <div className="chart-cell">
+          <ChartWrapper
+            title="Traders Profit vs. Loss"
+            loading={tradersLoading}
+            data={tradersData?.data}
+            csvFields={[{key: 'profit'}, {key: 'loss'}, {key: 'profitCumulative'}, {key: 'lossCumulative'}]}
+          >
+            <ResponsiveContainer width="100%" syncId="tradersId" height={CHART_HEIGHT}>
+              <ComposedChart data={tradersData?.data} barGap={0}>
+                <CartesianGrid strokeDasharray="10 10" />
+                <XAxis dataKey="timestamp" tickFormatter={tooltipLabelFormatter} minTickGap={30} />
+                <YAxis domain={[-tradersData?.stats.maxProfitLoss * 1.05, tradersData?.stats.maxProfitLoss * 1.05]} tickFormatter={yaxisFormatter} width={YAXIS_WIDTH} />
+                <YAxis domain={[-tradersData?.stats.maxCumulativeProfitLoss * 1.1, tradersData?.stats.maxCumulativeProfitLoss * 1.1]} orientation="right" yAxisId="right" tickFormatter={yaxisFormatter} width={YAXIS_WIDTH} />
+                <Tooltip
+                  formatter={tooltipFormatter}
+                  labelFormatter={tooltipLabelFormatter}
+                  contentStyle={{ textAlign: 'left' }}
+                />
+                <Legend />
+                <Area  yAxisId="right" type="monotone" stroke={0} fill="#88eba1" dataKey="profitCumulative" name="Cumulative Profit" />
+                <Area  yAxisId="right" type="monotone" stroke={0} fill="#f98888" dataKey="lossCumulative" name="Cumulative Loss" />
+                <Bar type="monotone" fill="#22c761" dot={false} dataKey="profit" name="Profit" />
+                <Bar type="monotone" fill="#f93333" dot={false} dataKey="loss" name="Loss" />
+              </ComposedChart>
+            </ResponsiveContainer>
+            <div className="chart-description">
+              <p>Considers settled (closed) positions</p>
+              <p>Fees are not factored into PnL</p>
+            </div>
+          </ChartWrapper>
+        </div>
+        <div className="chart-cell">
+           <GenericChart
+              loading={fundingRateLoading}
+              title="Borrowing Rate Annualized"
+              data={fundingRateData}
+              yaxisDataKey="ETH"
+              yaxisTickFormatter={yaxisFormatterPercent}
+              tooltipFormatter={tooltipFormatterPercent}
+              items={[{ key: 'WETH.e' }, { key: 'WBTC.e' }, { key: 'AVAX' }, { key: 'MIM' }, { key: 'USDC' }, { key: 'USDC.e' }]}
+              type="Line"
+              yaxisDomain={[0, 90 /* ~87% is a maximum yearly borrow rate */]}
+            />
         </div>
         <div className="chart-cell">
            <GenericChart
@@ -347,6 +441,14 @@ function Arbitrum() {
                 { key: 'uniqueCountCumulative', name: 'Cumulative', type: 'Line', yAxisId: 'right', strokeWidth: 2, color: COLORS[4] }
               ]}
               type="Composed"
+            />
+        </div>
+        <div className="chart-cell">
+           <GenericChart
+              loading={swapSourcesLoading}
+              title="Swap Sources"
+              data={swapSources}
+              items={swapSourcesKeys.map(key => ({ key }))}
             />
         </div>
       </div>
