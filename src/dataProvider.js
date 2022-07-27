f69d73c7d95fc5656867d54c34c893066e6cb74e
gdev8317 <95585424+gdev8317@users.noreply.github.com>
2022-07-27 12:46:42 +0300

    exclude MIM from borrowing rate chart

--- src/dataProvider.js
+++ src/dataProvider.js
@@ -837,6 +837,9 @@ export function useFundingRateData({ from = FIRST_DATE_TS, to = NOW_TS, chainNam
 
     const groups = graphData.fundingRates.reduce((memo, item) => {
       const symbol = tokenSymbols[item.token]
+      if (symbol === 'MIM') {
+        return memo
+      }
       memo[item.timestamp] = memo[item.timestamp] || {
         timestamp: item.timestamp
       }
