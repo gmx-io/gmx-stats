aef802553f6a4c992220430471dddd9dcc367756
gdev8317 <95585424+gdev8317@users.noreply.github.com>
2023-05-05 14:43:10 +0100

    restore stats.js from master

--- src/stats.js
+++ src/stats.js
@@ -1,11 +1,28 @@
 import { gql } from '@apollo/client'
 import { ethers } from 'ethers'
+import { getLogger } from './helpers'
 
 import TtlCache from './ttl-cache'
 import { getStatsClient } from './graph'
 import { ARBITRUM, AVALANCHE } from './addresses'
 
-const ttlCache = new TtlCache(60, 10)
+const CACHE_TTL = 300
+const ttlCache = new TtlCache(CACHE_TTL, 10)
+
+const logger = getLogger('stats')
+
+async function updateCache() {
+  try {
+    const start = Date.now()
+    await get24HourVolume(false);
+    logger.info('24h volume updated in %sms', Date.now() - start)
+  } catch (ex) {
+    logger.error(ex)
+  }
+  
+  setTimeout(updateCache, CACHE_TTL / 2 * 1000);
+}
+updateCache();
 
 async function get24HourVolumeForChain(chainId) {
   const client = getStatsClient(chainId);
@@ -34,12 +51,13 @@ async function get24HourVolumeForChain(chainId) {
   return volume;
 }
 
-export async function get24HourVolume() {
+export async function get24HourVolume(useCache = true) {
   const cached = ttlCache.get('24HourVolume')
-  if (cached) {
+  if (useCache && cached) {
     return cached
   }
   
+  logger.info('Requesting 24h volume from subgraphs')
   const [arbitrumVolume, avalancheVolume] = await Promise.all([
     get24HourVolumeForChain(ARBITRUM),
     get24HourVolumeForChain(AVALANCHE)
